6a881245d5ba34703de1bcac29e58499cd152ef3
ofeksabag <osabag55@gmail.com>
2022-06-26 22:12:12 +0300

    ChenRis

---
+++ main.js
@@ -0,0 +1,11 @@
+function showWind() {
+    $(".toolbar_content").css("display","block");
+    $(".toolbar").css("width","250px");
+}
+function offWind() {
+    $(".toolbar_content").css("display","none");
+    $(".toolbar").css("width","0px");
+}
+function letsgo() {
+    alert('לא ניתן לקבוע תורים כרגע.');
+}
