fa1fdcc3f6cceb1d23baa0bcf4926b3ea42cbbfb
ofeksabag <osabag55@gmail.com>
2022-08-26 21:39:36 +0300

    .

---
+++ main.js
@@ -0,0 +1,11 @@
+function showWind() {
+    $(".toolbar").css("display","block");
+    $("nav").css("width","250px");
+}
+function offWind() {
+    $(".toolbar").css("display","none");
+    $("nav").css("width","0px");
+}
+function letsGo() {
+    alert('לא ניתן לקבוע תורים כרגע.');
+}
